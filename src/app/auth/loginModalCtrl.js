b8f0b3933abbad482eb7023a2f88836c642dce39
Rares Oroian <oroian.rares@gmail.com>
2015-09-04 15:42:32 +0300

    Create login modal template, controller and add it to main.html.

--- src/app/auth/loginModalCtrl.js
+++ src/app/auth/loginModalCtrl.js
@@ -8,11 +8,47 @@
     .module('chat.auth')
     .controller('loginModalCtrl', loginModalCtrlFunc);
 
-  loginModalCtrlFunc.$inject = ['$scope'];
+  loginModalCtrlFunc.$inject = ['$scope', '$modal', '$log'];
 
   /* @ngInject */
-  function loginModalCtrlFunc($scope) {
+  function loginModalCtrlFunc($scope, $modal, $log) {
+    $scope.items = ['item1', 'item2', 'item3'];
+    $scope.animationsEnabled = true;
 
+    $scope.open = function (size) {
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'app/auth/loginModalTemplate.html',
+        controller: 'loginModalCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result
+        .then(function (selectedItem) {
+          // passes through here when the modal is closed
+          $log.info('Modal closed');
+        }, function () {
+          // passes through there when the modal is dismissed
+           $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.dismiss = function() {
+      $scope.$dismiss();
+    };
+
+    $scope.close = function() {
+      $scope.$close();
+    };
+
+    $scope.toggleAnimation = function () {
+      $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
   }
 
  })();
