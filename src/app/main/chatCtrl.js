7fca9ad8b286ec7a66f42317be2c258d38f2eb54
Rares Oroian <oroian.rares@gmail.com>
2015-09-04 10:36:30 +0300

    Change test controller to chatCtrl.

---
+++ src/app/main/chatCtrl.js
@@ -0,0 +1,11 @@
+angular
+  .module('chat')
+  .controller('chatCtrl', chatCtrlFunc);
+
+chatCtrlFunc.$inject = ['$scope'];
+
+/* @ngInject */
+function chatCtrlFunc($scope) {
+  /* jshint validthis: true */
+
+}
