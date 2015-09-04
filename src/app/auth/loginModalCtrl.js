d703ac1efe2c9b53bf8747f7a4c090117ec8c34e
Rares Oroian <oroian.rares@gmail.com>
2015-09-04 12:40:34 +0300

    Create login modal controller.

---
+++ src/app/auth/loginModalCtrl.js
@@ -0,0 +1,18 @@
+/**
+ * Created by rares on 04/09/15.
+ */
+(function loginModalCtrlIIFE() {
+    'use strict';
+
+  angular
+    .module('chat.auth')
+    .controller('loginModalCtrl', loginModalCtrlFunc);
+
+  loginModalCtrlFunc.$inject = ['$scope'];
+
+  /* @ngInject */
+  function loginModalCtrlFunc($scope) {
+
+  }
+
+ })();
