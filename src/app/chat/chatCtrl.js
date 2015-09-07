4cd3005750cadc3c7603977cea7a2b2140b2cfe2
Rares Oroian <oroian.rares@gmail.com>
2015-09-07 10:19:37 +0300

    Create basic chat controller.

--- src/app/chat/chatCtrl.js
+++ src/app/chat/chatCtrl.js
@@ -1,3 +1,18 @@
 /**
  * Created by rares on 07/09/15.
  */
+(function chatCtrlIIFE() {
+    'use strict';
+
+  angular
+    .module('AlgoTechChat.chat')
+    .controller('chatCtrl', chatCtrlFunc);
+
+  chatCtrlFunc.$inject = ['$scope'];
+
+  /* @ngInject */
+  function chatCtrlFunc($scope) {
+    $scope.message = 'hello!';
+  }
+
+ })();
