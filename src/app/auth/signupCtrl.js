87b76e9dc32898c988acc2ec715bd499f1b7d04c
Rares Oroian <oroian.rares@gmail.com>
2015-09-07 09:49:32 +0300

    Add login method with basic input check.

--- src/app/auth/signupCtrl.js
+++ src/app/auth/signupCtrl.js
@@ -13,8 +13,30 @@
   /* @ngInject */
   function signupCtrlFunc($scope) {
 
-    $scope.send = function(username, password) {
+    $scope.send = function(username, password, passwordRe) {
+      if (!username) {
+        $scope.invalidationMessage = "You must enter a valid username";
+        return;
+      }
 
+      if (!password) {
+        $scope.invalidationMessage = "You must enter a valid password";
+        return;
+      }
+
+      if (!passwordRe) {
+        $scope.invalidationMessage = "You must repeat your password";
+        return;
+      }
+
+      if (password != passwordRe) {
+        $scope.invalidationMessage = "Password do NOT match!";
+        user.password = "";
+        user.passwordRe = "";
+        return;
+      }
+
+      console.log('send call');
     }
   }
 
