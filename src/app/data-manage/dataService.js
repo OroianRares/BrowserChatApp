6daf931f364283ab6ca220687a71826681baf54d
Rares Oroian <oroian.rares@gmail.com>
2015-09-07 15:13:59 +0300

    Partially implement user related operations.

--- src/app/data-manage/dataService.js
+++ src/app/data-manage/dataService.js
@@ -1,3 +1,46 @@
 /**
  * Created by rares on 07/09/15.
  */
+(function dataServiceIIFE() {
+    'use strict';
+
+  angular
+    .module('AlgoMaskChat.data.manage')
+    .factory('dataService', dataServiceFunc);
+
+  dataServiceFunc.$inject = ['Restangular'];
+
+  /* @ngInject */
+  function dataServiceFunc(Restangular) {
+    var baseUsers = Restangular.all('users');
+    var service = {
+      updateUser: updateUser,
+      getUsers: getUsersList,
+      createAccount: createAccount,
+      getUser: getUser
+    };
+
+    return service;
+
+
+    function updateUser(user) {
+      return user.save();
+    }
+
+    function getUsersList() {
+      return baseUsers.all('accounts').getList().$object;
+    }
+
+    function createAccount(newAccount) {
+      return baseUsers.post(newAccount);
+    }
+
+    function getUser(token) {
+
+    }
+
+
+  }
+
+
+})();
