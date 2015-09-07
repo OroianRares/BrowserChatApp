144305a5fe2a43abce2e6329de9f57857d900e75
Rares Oroian <oroian.rares@gmail.com>
2015-09-07 15:45:48 +0300

    Add dataService and tokenService, sketch implementation of activate() and logout().

--- src/app/chat/chatCtrl.js
+++ src/app/chat/chatCtrl.js
@@ -8,12 +8,33 @@
     .module('AlgoTechChat.chat')
     .controller('chatCtrl', chatCtrlFunc);
 
-  chatCtrlFunc.$inject = ['$scope', 'dataService'];
+  chatCtrlFunc.$inject = ['$scope', 'dataService', 'tokenService'];
 
   /* @ngInject */
-  function chatCtrlFunc($scope, dataService) {
+  function chatCtrlFunc($scope, dataService, tokenService) {
     $scope.message = 'hello!';
-    dataService.getUsers();
+
+    activate();
+
+
+
+    function activate() {
+      var token = tokenService.getToken();
+
+      //dataService.getUser(token).then(
+      //  function(userObj) {
+      //    $scope.user = userObj;
+      //  }, function(err) {
+      //    $scope.getUserFailMsg = err.message;
+      //  });
+
+      //$scope.users = dataService.getUsers(token);
+    }
+
+    function logout() {
+      var token = tokenService.getToken();
+      dataService.logout(token);
+    }
   }
 
  })();
